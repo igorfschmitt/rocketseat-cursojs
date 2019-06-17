b29d82d8de4ff43800237814975e48debceb37bd
unknown <igor_f_schmitt@saojose.sc.senai.br>
2019-06-17 12:26:17 -0300

    fizemos a lista no js(certo)

--- Mod 3/Aula/3/todo.js
+++ Mod 3/Aula/3/todo.js
@@ -2,8 +2,6 @@ var listElement = document.querySelector("#app ul");
 var inputElement = document.querySelector("#app input");
 var buttonElement = document.querySelector("#app button");
 
-var todos = [
-    'Fazer Café',
-    'Beber Gasolina',
-    'Jogar lixo no lixo'
-];
+console.log(listElement);
+console.log(inputElement);
+console.log(buttonElement);
