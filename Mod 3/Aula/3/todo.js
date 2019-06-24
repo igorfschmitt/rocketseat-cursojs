4b8d9112fc8eb409a4fd8b6863e52ef9ecd0117e
unknown <igor_f_schmitt@saojose.sc.senai.br>
2019-06-24 11:47:16 -0300

    finalizado exercicio 3

--- Mod 3/Aula/3/todo.js
+++ Mod 3/Aula/3/todo.js
@@ -2,6 +2,20 @@ var listElement = document.querySelector("#app ul");
 var inputElement = document.querySelector("#app input");
 var buttonElement = document.querySelector("#app button");
 
-console.log(listElement);
-console.log(inputElement);
-console.log(buttonElement);
+var todos = [
+    'Fazer Café',
+    'Beber Gasolina',
+    'Jogar lixo no lixo'
+];
+
+function renderTodos() {
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText); 
+        listElement.appendChild(todoElement); 
+    } 
+}
+
+renderTodos();
